b2eca291d7dec2b18f96b28d223ba56dea361019
sohag282 <sohagshah570@gmail.com>
2022-06-06 11:19:51 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './component/Header/Header';
+import Shop from './component/Shop/Shop';
+
+function App() {
+  return (
+    <div className="App">
+     <Header/>
+     <Shop/>
+    </div>
+  );
+}
+
+export default App;
